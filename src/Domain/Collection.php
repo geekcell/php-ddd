6e0c0a9796daa8f5513ce7b179f8b347cbd33c86
Pascal Cremer <pascal.cremer@geekcell.io>
2023-01-20 12:48:09 +0100

    feat: Add more functionality to `Collection` class
    New functionality:
    - Collection now implements `ArrayAccess` interface.
    - New method `add` to add new elements to the collection.
    - Basic FP methods `map`, `filter`, and `reduce`.

--- src/Domain/Collection.php
+++ src/Domain/Collection.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace GeekCell\Ddd\Domain;
 
+use ArrayAccess;
 use ArrayIterator;
 use Assert;
 use Countable;
 use IteratorAggregate;
 use Traversable;
 
-class Collection implements Countable, IteratorAggregate
+class Collection implements ArrayAccess, Countable, IteratorAggregate
 {
     /**
      * @template T of object
@@ -18,10 +19,12 @@ class Collection implements Countable, IteratorAggregate
      *
      * @param T[] $items
      * @param class-string<T> $itemType
+     *
+     * @throws Assert\AssertionFailedException
      */
     final public function __construct(
         private readonly array $items = [],
-        ?string $itemType = null,
+        private ?string $itemType = null,
     ) {
         if ($itemType !== null) {
             Assert\Assertion::allIsInstanceOf($items, $itemType);
@@ -29,11 +32,126 @@ class Collection implements Countable, IteratorAggregate
     }
 
     /**
+     * Add one or more items to the collection. It **does not** modify the
+     * current collection, but returns a new one.
+     *
+     * @param mixed $item  One or more items to add to the collection.
+     * @return static
+     *
+     * @throws Assert\AssertionFailedException
+     */
+    public function add(mixed $item): static
+    {
+        if (!is_array($item)) {
+            $item = [$item];
+        }
+
+        if ($this->itemType !== null) {
+            Assert\Assertion::allIsInstanceOf($item, $this->itemType);
+        }
+
+        return new static([...$this->items, ...$item], $this->itemType);
+    }
+
+    /**
+     * Filter the collection using the given callback. It **does not** modify
+     * the current collection, but returns a new one.
+     *
+     * @param callable $callback  The callback to use for filtering.
+     * @return static
+     */
+    public function filter(callable $callback): static
+    {
+        return new static(
+            array_filter($this->items, $callback),
+            $this->itemType,
+        );
+    }
+
+    /**
+     * Map the collection using the given callback. It **does not** modify
+     * the current collection, but returns a new one.
+     *
+     * @param callable $callback  The callback to use for mapping.
+     * @param bool $inferTypes    Whether to infer the type of the items in the
+     *                            collection based on the first item in the
+     *                            mapping result. Defaults to `true`.
+     *
+     * @return static
+     */
+    public function map(callable $callback, bool $inferTypes = true): static
+    {
+        $mapResult = array_map($callback, $this->items);
+        $firstItem = reset($mapResult);
+
+        if ($firstItem === false || !is_object($firstItem)) {
+            return new static($mapResult);
+        }
+
+        if ($inferTypes && $this->itemType !== null) {
+            return new static($mapResult, get_class($firstItem));
+        }
+
+        return new static($mapResult);
+    }
+
+    /**
+     * Reduce the collection using the given callback.
+     *
+     * @param callable $callback  The callback to use for reducing.
+     * @param mixed $initial      The initial value to use for reducing.
+     *
+     * @return mixed
+     */
+    public function reduce(callable $callback, mixed $initial = null): mixed
+    {
+        return array_reduce($this->items, $callback, $initial);
+    }
+
+    /**
      * @inheritDoc
      */
-    public function getIterator(): Traversable
+    public function offsetExists(mixed $offset): bool
     {
-        return new ArrayIterator($this->items);
+        if (!is_int($offset)) {
+            return false;
+        }
+
+        return isset($this->items[$offset]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetGet(mixed $offset): mixed
+    {
+        if (!$this->offsetExists($offset)) {
+            return null;
+        }
+
+        return $this->items[$offset];
+    }
+
+    /**
+     * This method is not supported since it would break the immutability of the
+     * collection.
+     *
+     * @inheritDoc
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        // Unsupported since it would break the immutability of the collection.
+    }
+
+    /**
+     * This method is not supported since it would break the immutability of the
+     * collection.
+     *
+     * @inheritDoc
+     */
+    public function offsetUnset(mixed $offset): void
+    {
+        // Unsupported since it would break the immutability of the collection.
     }
 
     /**
@@ -42,5 +160,13 @@ class Collection implements Countable, IteratorAggregate
     public function count(): int
     {
         return count($this->items);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->items);
     }
 }
--- tests/Domain/CollectionTest.php
+++ tests/Domain/CollectionTest.php
@@ -17,6 +17,15 @@ class Foo
 {
 }
 
+/**
+ * Test fixture for Collection.
+ *
+ * @package GeekCell\Ddd\Tests\Domain
+ */
+class Bar
+{
+}
+
 class CollectionTest extends TestCase
 {
     public function testTypedConstructor(): void
@@ -53,6 +62,40 @@ class CollectionTest extends TestCase
         $this->assertInstanceOf(Collection::class, $collection);
     }
 
+    public function testArrayAccess(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+
+        // When
+        $collection = new Collection($items, Foo::class);
+
+        // Then
+        $this->assertEquals($collection[0], $items[0]);
+        $this->assertEquals($collection[1], $items[1]);
+        $this->assertEquals($collection[2], $items[2]);
+        $this->assertFalse(isset($collection[3]));
+    }
+
+    public function testArrayAccessWithInvalidOffset(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+        $collection = new Collection($items, Foo::class);
+
+        // When
+        $result1 = $collection[10];
+        $result2 = $collection[-1];
+        $result3 = $collection[0.5];
+        $result4 = $collection['foo'];
+
+        // When
+        $this->assertNull($result1);
+        $this->assertNull($result2);
+        $this->assertNull($result3);
+        $this->assertNull($result4);
+    }
+
     public function testCount(): void
     {
         // Given
@@ -76,5 +119,170 @@ class CollectionTest extends TestCase
         // Then
         $this->assertEquals($items, iterator_to_array($collection));
         $this->assertCount(3, iterator_to_array($collection));
+    }
+
+    public function testAdd(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+        $collection = new Collection($items, Foo::class);
+
+        // When
+        $newCollection = $collection->add(new Foo());
+
+        // Then
+        $this->assertCount(4, $newCollection);
+        $this->assertCount(3, $collection);
+        $this->assertNotSame($collection, $newCollection);
+    }
+
+    public function testAddMultiple(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+        $collection = new Collection($items, Foo::class);
+
+        // When
+        $newCollection = $collection->add([new Foo(), new Foo()]);
+
+        // Then
+        $this->assertCount(5, $newCollection);
+        $this->assertCount(3, $collection);
+        $this->assertNotSame($collection, $newCollection);
+    }
+
+    public function testFilter(): void
+    {
+        // Given
+        $items = [1, 2, 3,4, 5, 6, 7, 8, 9, 10];
+        $collection = new Collection($items);
+
+        // When
+        $newCollection = $collection->filter(fn (int $i) => $i % 2 === 0);
+
+        // Then
+        $this->assertCount(5, $newCollection);
+        $this->assertCount(10, $collection);
+        $this->assertNotSame($collection, $newCollection);
+    }
+
+    public function testMap(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+        $collection = new Collection($items, Foo::class);
+
+        // When
+        $newCollection = $collection->map(fn (Foo $item) => new Bar());
+
+        // Then
+        $this->assertCount(3, $newCollection);
+        $this->assertCount(3, $collection);
+        $this->assertNotSame($collection, $newCollection);
+    }
+
+    public function testMapWithStrictTypes(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+        $collection = new Collection($items, Foo::class);
+
+        // Then
+        $this->expectException(Assert\InvalidArgumentException::class);
+
+        // When
+        $counter = 0;
+        $newCollection = $collection->map(
+            function (Foo $item) use (&$counter) {
+                if ($counter++ % 2) {
+                    return new Bar();
+                }
+
+                return $item;
+            },
+            true,
+        );
+    }
+
+    public function testMapWithoutStrictTypes(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+        $collection = new Collection($items, Foo::class);
+
+        // When
+        $counter = 0;
+        $newCollection = $collection->map(
+            function ($item) use (&$counter) {
+                if ($counter++ % 2) {
+                    return new Bar();
+                }
+
+                return $item;
+            },
+            false,
+        );
+
+        // Then
+        $this->assertCount(3, $newCollection);
+        $this->assertCount(3, $collection);
+        $this->assertNotSame($collection, $newCollection);
+    }
+
+    public function testMapWithStrictTypesAndScalars(): void
+    {
+        // Given
+        $items = [new Foo(), new Foo(), new Foo()];
+        $collection = new Collection($items, Foo::class);
+
+        // When
+        $counter = 0;
+        $newCollection = $collection->map(
+            function (Foo $item) use (&$counter) {
+                if ($counter % 2) {
+                    return $counter++;
+                }
+
+                return 'foo';
+            },
+            true,
+        );
+
+        // Then
+        $this->assertCount(3, $newCollection);
+        $this->assertCount(3, $collection);
+        $this->assertNotSame($collection, $newCollection);
+    }
+
+    public function testReduce(): void
+    {
+        // Given
+        $items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        $collection = new Collection($items);
+
+        // When
+        $result = $collection->reduce(
+            fn (int $carry, int $item) => $carry + $item,
+            0,
+        );
+
+        // Then
+        $this->assertEquals(55, $result);
+    }
+
+    public function testChaining(): void
+    {
+        // Given
+        $items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        $collection = new Collection($items);
+
+        // When
+        $result = $collection
+            ->filter(fn (int $i) => $i % 2 === 0)
+            ->map(fn (int $i) => $i * 2)
+            ->reduce(fn (int $carry, int $item) => $carry + $item, 0);
+
+        // Then
+        $this->assertEquals(60, $result);
     }
 }
